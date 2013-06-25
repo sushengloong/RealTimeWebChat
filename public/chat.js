e601b40fa1034bb8fa361dd5568641ed310d8a28
Sheng-Loong Su <sushengloong@gmail.com>
2013-06-25 22:54:47 +0800

    clear message field after sending

--- public/chat.js
+++ public/chat.js
@@ -27,6 +27,7 @@ window.onload = function() {
       } else {
         var text = field.value;
         socket.emit('send', { message: text, username: name.value });
+        field.value = "";
       }
     };
  
